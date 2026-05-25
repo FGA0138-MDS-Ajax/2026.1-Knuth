76826d775f20ea6783a29888ef0311d39f3ef340
Ester Cabral <esteralmeida1997@gmail.com>
2026-05-24 22:50:24 -0300

    feat: adiciona arquivos da dashboard e atualiza login e estilos

--- frontend/login.js
+++ frontend/login.js
@@ -5,6 +5,22 @@ form.addEventListener("submit", (e) => {
 
     e.preventDefault();
 
+    
+    const email = form.querySelector('input[type="email"]').value;
+    // Extrai o nome antes do '@' apenas para simular o nome da pessoa
+    const nomeIdentificado = email.split('@')[0]; 
     alert("Login realizado!");
+        // token
+    localStorage.setItem("access_token", "token_simulado_123");
+    
+    // Leva o usuário para a dashboard
+    window.location.href = "dashboard.html";
+
 
 });
+
+
+
+   
+
+    
