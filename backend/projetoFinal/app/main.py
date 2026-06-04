02e83f5e9caaa6b9d3fcb444aef7cdffd54d3792
smatheusnf <mnunes.ferreirasilva@gmail.com>
2026-06-04 20:57:38 -0300

    Criacao CRUD professores

--- backend/projetoFinal/app/main.py
+++ backend/projetoFinal/app/main.py
@@ -3,16 +3,19 @@ import uvicorn
 from app.api.v1.routes.curso_route import router as curso_router
 from app.api.v1.routes.aluno_route import router as aluno_router
 from app.api.v1.routes.usuario_route import router as usuario_router
-from app.api.v1.routes.auth_route import router as auth_router           
+from app.api.v1.routes.auth_route import router as auth_router
 from app.api.v1.routes.turma_route import router as turma_router
-from app.core.database import create_db_and_tables  
+from app.api.v1.routes.professor_route import router as professor_router
+from app.core.database import create_db_and_tables
 from contextlib import asynccontextmanager
 from app.core.config import settings
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     create_db_and_tables()
     yield
+
 
 app = FastAPI(title="API", lifespan=lifespan)
 app.include_router(curso_router)
@@ -20,6 +23,7 @@ app.include_router(aluno_router)
 app.include_router(usuario_router)
 app.include_router(auth_router)
 app.include_router(turma_router)
+app.include_router(professor_router)
 
 if __name__ == "__main__":
     uvicorn.run("app.main:app", host="0.0.0.0", port=settings.SERVER_PORT, reload=True)
