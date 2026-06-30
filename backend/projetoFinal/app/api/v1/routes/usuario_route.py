daa6436967e604869d1a07f1d22f5957187b1b87
Tomas Garcia Rocho <tomasgarciarocho@gmail.com>
2026-06-29 22:21:49 -0300

    fix: tirando permissoes desnecessarias

--- backend/projetoFinal/app/api/v1/routes/usuario_route.py
+++ backend/projetoFinal/app/api/v1/routes/usuario_route.py
@@ -12,26 +12,26 @@ router = APIRouter(prefix="/usuarios", tags=["Usuários"])
 def create_usuario(usuario_create: UsuarioCreate, session: SessionDependency):
     return UsuarioService.create(session, usuario_create)
 
-@router.get("/username/{username}", response_model=UsuarioRead, summary="Obter um usuário por username", dependencies=[Depends(possui_permissao(["ADMIN"]))])
+@router.get("/username/{username}", response_model=UsuarioRead, summary="Obter um usuário por username")
 def get_usuario_by_username(username: str, session: SessionDependency):
     return UsuarioService.get_by_username(session, username)
 
-@router.get("/{usuario_id}", response_model=UsuarioRead, summary="Obter um usuário por ID", dependencies=[Depends(possui_permissao(["ADMIN"]))])
+@router.get("/{usuario_id}", response_model=UsuarioRead, summary="Obter um usuário por ID")
 def get_usuario_by_id(usuario_id: int, session: SessionDependency):
     return UsuarioService.get_by_id(session, usuario_id)
 
-@router.delete("/{usuario_id}", summary="Deletar um usuário", dependencies=[Depends(possui_permissao(["ADMIN"]))])
+@router.delete("/{usuario_id}", summary="Deletar um usuário")
 def delete_usuario(usuario_id: int, session: SessionDependency):
     return UsuarioService.delete(session, usuario_id)
 
-@router.get("/", response_model=list[UsuarioRead], summary="Obter todos os usuários", dependencies=[Depends(possui_permissao(["ADMIN"]))])
+@router.get("/", response_model=list[UsuarioRead], summary="Obter todos os usuários")
 def get_all_usuarios(session: SessionDependency):
     return UsuarioService.get_all(session)
 
-@router.put("/senha",response_model=UsuarioRead, summary="Trocar senha do usuário", dependencies=[Depends(get_current_username)])
+@router.put("/senha",response_model=UsuarioRead, summary="Trocar senha do usuário")
 def update_password(troca_senha: UsuarioTrocaSenha, session: SessionDependency):
     return UsuarioService.update_password(session, troca_senha)
 
-@router.put("/{usuario_id}", response_model=UsuarioRead, summary="Atualizar informações de um usuário", dependencies=[Depends(possui_permissao(["ADMIN"]))])
+@router.put("/{usuario_id}", response_model=UsuarioRead, summary="Atualizar informações de um usuário")
 def update_usuario(usuario_id: int, usuario_update: UsuarioUpdate, session: SessionDependency):
     return UsuarioService.update_usuario(session, usuario_id, usuario_update)
